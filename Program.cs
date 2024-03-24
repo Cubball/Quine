4099696cca0055f6cb985276e1d0e5ab263bf3ca
Cubball <138378823+Cubball@users.noreply.github.com>
2024-03-24 10:07:47 +0200

    Create a quine using Program.Main

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Console.WriteLine({0}, @{1}{1}{0}{1}{1}, @{1}{1}{1}{1});", @"""Console.WriteLine({0}, @{1}{1}{0}{1}{1}, @{1}{1}{1}{1});""", @"""");
+﻿internal class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("internal class Program\n{{\n    public static void Main()\n    {{\n        Console.WriteLine({0}, @{1}{1}{0}{1}{1}, @{1}{1}{1}{1});\n    }}\n}}", @"""internal class Program\n{{\n    public static void Main()\n    {{\n        Console.WriteLine({0}, @{1}{1}{0}{1}{1}, @{1}{1}{1}{1});\n    }}\n}}""", @"""");
+    }
+}
